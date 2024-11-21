f35aa0e1c0bff73b0583612bbfd31ee4f324f626
b.kumarbaiuulu <b.kumarbaiuulu>
2024-11-21 14:47:21 +0600

    push

--- src/RateLimiter.php
+++ src/RateLimiter.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace RateLimiter\RateLimiter;
 
 use RateLimiter\RateLimiter\Exception\RateLimitExceededException;
@@ -8,44 +7,59 @@ use Redis;
 class RateLimiter
 {
     private Redis $redis;
+    private RateLimiterConfig $config;
 
-    public function __construct(Redis $redis)
+    public function __construct(Redis $redis, RateLimiterConfig $config)
     {
         $this->redis = $redis;
+        $this->config = $config;
     }
 
     /**
-     * @param string $key
-     * @param RateLimiterConfig $config
+     *
+     * @param string $key Уникальный ключ
      * @return void
-     * @throws RateLimitExceededException
+     * @throws RateLimitExceededException Если лимит превышен
      */
-    public function checkRateLimit(string $key, RateLimiterConfig $config): void
+    public function checkRateLimit(string $key): void
     {
         $attemptsKey = $key . ':attempts';
         $blockKey = $key . ':blocked';
+        $lastAttemptKey = $key . ':last_attempt_time';
 
-        // Проверка блокировки
+
         if ($this->redis->exists($blockKey)) {
-            throw new RateLimitExceededException("Too many attempts. Try again later.");
+            $remainingTime = $this->redis->ttl($blockKey);
+            throw new RateLimitExceededException("You are blocked. Try again in $remainingTime seconds.");
         }
 
-        // Получение количества попыток
-        $attempts = $this->redis->get($attemptsKey) ?: 0;
+        $lastAttemptTime = (int)$this->redis->get($lastAttemptKey);
+        $currentTime = time();
 
-        if ($attempts >= $config->getMaxAttempts()) {
-            // Установить блокировку
-            $this->redis->set($blockKey, 1, $config->getBlockDuration());
+        if ($lastAttemptTime > 0 && ($currentTime - $lastAttemptTime) < $this->config->getRetryInterval()) {
+            $remainingTime = $this->config->getRetryInterval() - ($currentTime - $lastAttemptTime);
+            throw new RateLimitExceededException("Too many attempts. Try again in $remainingTime seconds.");
+        }
+
+        $attempts = (int)$this->redis->get($attemptsKey) ?: 0;
+
+        if ($attempts >= $this->config->getMaxAttempts()) {
+            $this->redis->set($blockKey, 1, $this->config->getBlockDuration());
             $this->redis->del($attemptsKey);
-            throw new RateLimitExceededException("Too many attempts. Try again later.");
+            $remainingTime = $this->redis->ttl($blockKey);
+            throw new RateLimitExceededException("Too many attempts. Try again in $remainingTime seconds.");
         }
 
-        // Увеличение счётчика попыток
         $this->redis->incr($attemptsKey);
 
-        // Установить TTL для ключа попыток
+
+        $this->redis->set($lastAttemptKey, $currentTime);
+
+
         if ($this->redis->ttl($attemptsKey) === -1) {
-            $this->redis->expire($attemptsKey, $config->getRetryInterval());
+            $this->redis->expire($attemptsKey, $this->config->getRetryInterval());
         }
     }
 }
+
+
